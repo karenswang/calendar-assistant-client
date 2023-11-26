8c5de7ddf9126184582590c6382db2db26dfc0ac
Karen Wang <46947530+karenswang@users.noreply.github.com>
2023-11-26 14:54:40 -0500

    implement authenticate endpoint

--- assets/login.js
+++ assets/login.js
@@ -1,33 +1,28 @@
-$(document).ready(function() {
-    $('#login_button').on('click', function() {
-        // Collect data from the form
-        var email = $('#email').val();
-        var clientId = $('#clientId').val();
-        var clientSecret = $('#clientSecret').val();
-        var openAIKey = $('#openAIKey').val();
+$('#login_button').on('click', function() {
+    var email = $('#email').val();
+    var clientId = $('#clientId').val();
+    var clientSecret = $('#clientSecret').val();
+    var openAIKey = $('#openAIKey').val();
 
-        // Prepare the request body
-        var data = {
-            email: email,
-            clientId: clientId,
-            clientSecret: clientSecret,
-            openAIKey: openAIKey
-        };
+    var data = {
+        email: email,
+        clientId: clientId,
+        clientSecret: clientSecret,
+        openAIKey: openAIKey
+    };
 
-        // Make AJAX request to the /authenticate endpoint
-        $.ajax({
-            url: 'http://localhost:3000/authenticate',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            success: function(response) {
-                // Handle success (you can store tokens or any response as needed)
-                console.log("Login successful: ", response);
-            },
-            error: function(xhr, status, error) {
-                // Handle errors
-                console.error("Error in login: ", status, error);
-            }
-        });
+    $.ajax({
+        url: 'http://localhost:3000/authenticate',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function(response) {
+            var userEmail = $('#email').val(); // Get the user's email from input field
+            $('.login_form').html('<p>Logged in: ' + userEmail + '</p>'); // Replace HTML
+            console.log("OAuth flow initiated");
+        },
+        error: function(xhr, status, error) {
+            console.error("Error in initiating authentication: ", status, error);
+        }
     });
 });
